aa77427048b992c46da62a7d3d23228803af4c5c
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2025-04-27 23:30:28 -0300

    08 de 20

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ import security
 from app import FuncionarioDAO
 from app import ClienteDAO
 from app import ProdutoDAO
+from app import ComandaDAO
 
 from contextlib import asynccontextmanager
 @asynccontextmanager
@@ -37,6 +38,26 @@ app.include_router(security.router)
 app.include_router(FuncionarioDAO.router)
 app.include_router(ClienteDAO.router)
 app.include_router(ProdutoDAO.router)
+app.include_router(ComandaDAO.router)
+
+# if __name__ == "__main__":
+#     uvicorn.run('main:app', host=HOST, port=int(PORT), reload=RELOAD)
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', host=HOST, port=int(PORT), reload=RELOAD)
+    import ssl
+    import hypercorn.asyncio
+    from hypercorn.config import Config
+    import asyncio
+
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.load_cert_chain(certfile="cert/cert.pem", keyfile="cert/ecc-key.pem")
+
+    config = Config()
+    config.bind = ["0.0.0.0:4443"]
+    config.quic_bind = ["0.0.0.0:4443"]
+    #config.insecure_bind = ["0.0.0.0:8000"]
+    config.certfile = "cert/cert.pem"
+    config.keyfile = "cert/ecc-key.pem"
+    config.alpn_protocols = ["h2","h3"]
+    
+    asyncio.run(hypercorn.asyncio.serve(app, config))
